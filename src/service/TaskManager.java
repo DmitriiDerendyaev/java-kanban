7f5c3d9f6990de3011176592bd04bfd78196c936
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-12 00:07:04 +0300

    added create method

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -14,6 +14,28 @@ public class TaskManager {
     public HashMap<Integer, Task> task = new HashMap<>();
     public HashMap<Integer, SubTask> subTask = new HashMap<>();
 
+    int currentId = 1;
+
+    public int createEpic(Epic newEpic){
+        newEpic.setTaskID(currentId++);
+        epic.put(newEpic.getTaskID(), newEpic);
+
+        return newEpic.getTaskID();
+    }
+
+    public int createTask(Task newTask){
+        newTask.setTaskID(currentId++);
+        task.put(newTask.getTaskID(), newTask);
+
+        return newTask.getTaskID();
+    }
+
+    public int createSubTask(SubTask newSubTask){
+        newSubTask.setTaskID(currentId++);
+        subTask.put(newSubTask.getEpicID(), newSubTask);
+
+        return newSubTask.getTaskID();
+    }
 
 
 }
