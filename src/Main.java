96b6de273ad17eb3f0ec5d4461751fcc2ef5f7eb
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-12 19:52:01 +0300

    test: добавление тестов для проверки добавления, обновления, удаления(без обновления статуса эпика)

--- src/Main.java
+++ src/Main.java
@@ -10,26 +10,44 @@ public class Main {
 
         TaskManager taskManager = new TaskManager();
 
+        //Основной блок теста по ТЗ
+        ///////////////////////////////////////////////////////
         Task firstTask = new Task("Просто отвертка", "Купить отвертку", TaskStatus.NEW);            // ID = 1
         Task secondTask = new Task("Просто шуруп", "Купить шуруп", TaskStatus.IN_PROGRESS);         // ID = 2
         Task thirdTask = new Task("Просто доска", "Купить доску", TaskStatus.NEW);                  // ID = 3
-        taskManager.createTask(firstTask);
-        taskManager.createTask(secondTask);
-        taskManager.createTask(thirdTask);
+        System.out.println("Добавление задачи: ID = " + taskManager.createTask(firstTask));
+        System.out.println("Добавление задачи: ID = " + taskManager.createTask(secondTask));
+        System.out.println("Добавление задачи: ID = " + taskManager.createTask(thirdTask));
 
         Epic firstEpic = new Epic("Купить дом", "Купить пентхаус в Казани", TaskStatus.NEW);        // ID = 4
-        taskManager.createEpic(firstEpic);
+        System.out.println("Добавление эпика: ID = " + taskManager.createEpic(firstEpic));
 
         SubTask firstSubTask = new SubTask("Материал для пола", "Покупка расходников для пола",
                 TaskStatus.NEW, 4);                                                                                // ID = 5
-        taskManager.createSubTask(firstSubTask);
+        System.out.println("Добавление подзадачи: ID = " + taskManager.createSubTask(firstSubTask));
 
         Epic secondEpic = new Epic("Купить машину", "Купить Porsche 911", TaskStatus.NEW);          // ID = 6
-        taskManager.createEpic(secondEpic);
+        System.out.println("Добавление эпика: ID = " + taskManager.createEpic(secondEpic));
 
         SubTask secondSubTask = new SubTask("Купить материал для потолка",
                 "Покупка расходников для потолка", TaskStatus.NEW, 4);                                // ID = 7
-        taskManager.createSubTask(secondSubTask);
+        System.out.println("Добавление подзадачи: ID = " + taskManager.createSubTask(secondSubTask));
+        ///////////////////////////////////////////////////////////
+
+        //Обновление задач
+        Task updSecondTask = new Task("Просто шуруп", "Купить шуруп", 2, TaskStatus.DONE);
+        taskManager.updateTask(updSecondTask);
+
+        SubTask updFirstSubTask = new SubTask("Материал для пола", "Покупка расходников для пола",
+                5, TaskStatus.DONE, 4);
+        taskManager.updateSubTask(updFirstSubTask);
+
+        //Удаление задач
+//        taskManager.removeEpicByID(6);
+
+        taskManager.removeSubTaskByID(5);
+
+        taskManager.clearEpic();
         System.out.println("All test passed");
     }
 }
