545ef661649fcb843696ed13640b56d46cc1136a
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-12 18:01:19 +0300

    failed tests

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,6 @@ public class Main {
         taskManager.addEpic();
 
         ArrayList<Task> gotSubTasks = taskManager.getTaskList();
-        System.out.println("All test passed");
+        System.out.println("All test failed");
     }
 }
