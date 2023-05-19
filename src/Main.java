aa89f8ac1367a4770c648ba5d1b48dfc0ef72278
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-05-19 20:15:07 +0300

    refactor: изменить расположение файлов и переименовать вызов класса InMemoryTaskManager

--- src/Main.java
+++ src/Main.java
@@ -2,77 +2,77 @@ import models.Epic;
 import models.SubTask;
 import models.Task;
 import models.TaskStatus;
-import service.TaskManager;
+import service.InMemoryTaskManager;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        TaskManager taskManager = new TaskManager();
+        InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
 
         //Основной блок теста по ТЗ #1
         ///////////////////////////////////////////////////////
         Task firstTask = new Task("Просто отвертка", "Купить отвертку", TaskStatus.NEW);            // ID = 1
         Task secondTask = new Task("Просто шуруп", "Купить шуруп", TaskStatus.NEW);         // ID = 2
         Task thirdTask = new Task("Просто доска", "Купить доску", TaskStatus.NEW);                  // ID = 3
-        System.out.println("Добавление задачи: ID = " + taskManager.createTask(firstTask));
-        System.out.println("Добавление задачи: ID = " + taskManager.createTask(secondTask));
-        System.out.println("Добавление задачи: ID = " + taskManager.createTask(thirdTask));
+        System.out.println("Добавление задачи: ID = " + inMemoryTaskManager.createTask(firstTask));
+        System.out.println("Добавление задачи: ID = " + inMemoryTaskManager.createTask(secondTask));
+        System.out.println("Добавление задачи: ID = " + inMemoryTaskManager.createTask(thirdTask));
 
         Epic firstEpic = new Epic("Купить дом", "Купить пентхаус в Казани", TaskStatus.NEW);        // ID = 4
-        System.out.println("Добавление эпика: ID = " + taskManager.createEpic(firstEpic));
+        System.out.println("Добавление эпика: ID = " + inMemoryTaskManager.createEpic(firstEpic));
 
         SubTask firstSubTask = new SubTask("Материал для пола", "Покупка расходников для пола",
                 TaskStatus.NEW, 4);                                                                                // ID = 5
-        System.out.println("Добавление подзадачи: ID = " + taskManager.createSubTask(firstSubTask));
+        System.out.println("Добавление подзадачи: ID = " + inMemoryTaskManager.createSubTask(firstSubTask));
 
         Epic secondEpic = new Epic("Купить машину", "Купить Porsche 911", TaskStatus.NEW);          // ID = 6
-        System.out.println("Добавление эпика: ID = " + taskManager.createEpic(secondEpic));
+        System.out.println("Добавление эпика: ID = " + inMemoryTaskManager.createEpic(secondEpic));
 
         SubTask secondSubTask = new SubTask("Купить материал для потолка",
                 "Покупка расходников для потолка", TaskStatus.NEW, 4);                                // ID = 7
-        System.out.println("Добавление подзадачи: ID = " + taskManager.createSubTask(secondSubTask));
+        System.out.println("Добавление подзадачи: ID = " + inMemoryTaskManager.createSubTask(secondSubTask));
 
         SubTask thirdSubTask = new SubTask("Купить спойлер", "Купить спойлер белого цвета",         // ID = 8
                 TaskStatus.NEW, 6);
-        System.out.println("Добавление подзадачи: ID = " + taskManager.createSubTask(thirdSubTask));
+        System.out.println("Добавление подзадачи: ID = " + inMemoryTaskManager.createSubTask(thirdSubTask));
 
-        System.out.println(taskManager.getTaskList());
-        System.out.println(taskManager.getSubTaskList());
-        System.out.println(taskManager.getEpicList());
+        System.out.println(inMemoryTaskManager.getTaskList());
+        System.out.println(inMemoryTaskManager.getSubTaskList());
+        System.out.println(inMemoryTaskManager.getEpicList());
         System.out.println("\n");
         ///////////////////////////////////////////////////////////
 
         //Обновление задач
         Task updSecondTask = new Task("Просто шуруп", "Купить шуруп", 2, TaskStatus.IN_PROGRESS);
-        taskManager.updateTask(updSecondTask);
+        inMemoryTaskManager.updateTask(updSecondTask);
 
         SubTask updFirstSubTask = new SubTask("Материал для пола", "Покупка расходников для пола",
                 5, TaskStatus.DONE, 4);
-        taskManager.updateSubTask(updFirstSubTask);
+        inMemoryTaskManager.updateSubTask(updFirstSubTask);
 
         SubTask updSecondSubTask = new SubTask("Купить материал для потолка",
                 "Покупка расходников для потолка", 7, TaskStatus.IN_PROGRESS, 4);
-        taskManager.updateSubTask(updSecondSubTask);
+        inMemoryTaskManager.updateSubTask(updSecondSubTask);
 
         SubTask updThirdSubTask = new SubTask("Купить спойлер", "Купить спойлер белого цвета",         // ID = 8
                 8, TaskStatus.IN_PROGRESS, 6);
-        taskManager.updateSubTask(updThirdSubTask);
+        inMemoryTaskManager.updateSubTask(updThirdSubTask);
 
-        System.out.println(taskManager.getTaskList());
-        System.out.println(taskManager.getSubTaskList());
-        System.out.println(taskManager.getEpicList());
+        System.out.println(inMemoryTaskManager.getTaskList());
+        System.out.println(inMemoryTaskManager.getSubTaskList());
+        System.out.println(inMemoryTaskManager.getEpicList());
         System.out.println("\n");
         ///////////////////////////////////////////////////////
 
         //Удаление задач
-        taskManager.removeTaskByID(3);
-        taskManager.removeSubTaskByID(7);
-        taskManager.removeEpicByID(6);
+        inMemoryTaskManager.removeTaskByID(3);
+        inMemoryTaskManager.removeSubTaskByID(7);
+        inMemoryTaskManager.removeEpicByID(6);
 
-        System.out.println(taskManager.getTaskList());
-        System.out.println(taskManager.getSubTaskList());
-        System.out.println(taskManager.getEpicList());
+        System.out.println(inMemoryTaskManager.getTaskList());
+        System.out.println(inMemoryTaskManager.getSubTaskList());
+        System.out.println(inMemoryTaskManager.getEpicList());
         System.out.println("\n");
 
         System.out.println("All test passed");
---
+++ src/service/InMemoryTaskManager.java
@@ -0,0 +1,206 @@
+package service;
+
+import models.Epic;
+import models.SubTask;
+import models.Task;
+import models.TaskStatus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class InMemoryTaskManager implements TaskManager{
+
+    protected HashMap<Integer, Epic> epics = new HashMap<>();
+    protected HashMap<Integer, Task> tasks = new HashMap<>();
+    protected HashMap<Integer, SubTask> subTasks = new HashMap<>();
+
+    int currentId = 1;
+
+
+
+    @Override
+    public int createEpic(Epic newEpic){
+        newEpic.setTaskID(currentId++);
+        epics.put(newEpic.getTaskID(), newEpic);
+
+        return newEpic.getTaskID();
+    }
+
+    @Override
+    public int createTask(Task newTask){
+        newTask.setTaskID(currentId++);
+        tasks.put(newTask.getTaskID(), newTask);
+
+        return newTask.getTaskID();
+    }
+
+    @Override
+    public int createSubTask(SubTask newSubTask){
+        newSubTask.setTaskID(currentId++);
+        epics.get(newSubTask.getEpicID()).addSubTaskID(newSubTask.getTaskID());
+        subTasks.put(newSubTask.getTaskID(), newSubTask);
+
+        updateEpicStatus(epics.get(newSubTask.getEpicID()));
+
+        return newSubTask.getTaskID();
+    }
+
+    @Override
+    public int updateTask(Task updatedTask){
+        tasks.put(updatedTask.getTaskID(), updatedTask);
+
+        return updatedTask.getTaskID();
+    }
+
+    @Override
+    public int updateSubTask(SubTask updatedSubTask){
+        subTasks.put(updatedSubTask.getTaskID(), updatedSubTask);
+        updateEpicStatus(epics.get(updatedSubTask.getEpicID()));
+
+        return updatedSubTask.getTaskID();
+    }
+
+    @Override
+    public Task getTaskByID(Integer taskID){
+        return tasks.get(taskID);
+    }
+
+        public SubTask getSubTaskByID(Integer subTaskID){
+        return subTasks.get(subTaskID);
+    }
+
+    @Override
+    public Epic getEpicByID(Integer epicId){
+        return epics.get(epicId);
+    }
+
+    @Override
+    public int removeTaskByID(Integer taskID){
+        tasks.remove(taskID);
+
+        return taskID;
+    }
+
+    @Override
+    public int removeSubTaskByID(Integer subTaskID){
+        SubTask subtask = subTasks.remove(subTaskID);
+        epics.get(subtask.getEpicID()).getTaskCollection().remove(subTaskID);
+
+        updateEpicStatus(epics.get(subtask.getEpicID()));
+        return subTaskID;
+    }
+
+    @Override
+    public void removeEpicByID(Integer epicId){
+        epics.remove(epicId);
+        Iterator<Integer> subTaskIterator = subTasks.keySet().iterator();
+        while(subTaskIterator.hasNext()){
+            Integer currentSubTask = subTaskIterator.next();
+            if(subTasks.get(currentSubTask).getEpicID() == epicId){
+                subTaskIterator.remove();
+            }
+        }
+    }
+
+    @Override
+    public void clearTasks(){
+        tasks.clear();
+    }
+
+    @Override
+    public void clearSubTasks(){
+        subTasks.clear();
+
+        for(Integer currentEpic: epics.keySet()){
+            epics.get(currentEpic).getTaskCollection().clear();
+        }
+
+        updateEpicsStatus();
+    }
+
+    @Override
+    public void clearEpics(){
+        epics.clear();
+        subTasks.clear();
+    }
+
+    @Override
+    public ArrayList<Task> getTaskList() {
+        ArrayList<Task> tasksList = new ArrayList<>(tasks.values());
+        return tasksList;
+    }
+
+    @Override
+    public ArrayList<SubTask> getSubTaskList(){
+        ArrayList<SubTask> subTasksList = new ArrayList<>(subTasks.values());
+        return subTasksList;
+    }
+
+    @Override
+    public ArrayList<Epic> getEpicList(){
+        ArrayList<Epic> epicsList = new ArrayList<>(epics.values());
+        return epicsList;
+    }
+
+    @Override
+    public ArrayList<SubTask> getSubTaskListByEpicID(Integer epicId){
+        ArrayList<SubTask> newSubTaskList = new ArrayList<>();
+
+        for(Integer currentSubTask: epics.get(epicId).getTaskCollection()){
+            newSubTaskList.add(subTasks.get(currentSubTask));
+        }
+        return newSubTaskList;
+    }
+
+    private void updateEpicsStatus(){
+        for(Integer currentEpic: epics.keySet()){
+            boolean isEverythingNew = true;
+            boolean isEverythingDone = true;
+            if(epics.get(currentEpic).getTaskCollection().isEmpty()){
+                epics.get(currentEpic).setStatus(TaskStatus.NEW);
+                continue;
+            }
+            for (Integer currentSubTask : epics.get(currentEpic).getTaskCollection()) {
+                if(!subTasks.get(currentSubTask).getTaskStatus().equals(TaskStatus.NEW)){
+                    isEverythingNew = false;
+                }
+                if(!subTasks.get(currentSubTask).getTaskStatus().equals(TaskStatus.DONE)){
+                    isEverythingDone = false;
+                }
+            }
+            if(isEverythingNew){
+                epics.get(currentEpic).setStatus(TaskStatus.NEW);
+            } else if (isEverythingDone) {
+                epics.get(currentEpic).setStatus(TaskStatus.DONE);
+            } else {
+                epics.get(currentEpic).setStatus(TaskStatus.IN_PROGRESS);
+            }
+        }
+    }
+
+    private void updateEpicStatus(Epic currentEpic){
+        boolean isEverythingNew = true;
+        boolean isEverythingDone = true;
+        if(currentEpic.getTaskCollection().isEmpty()){
+            currentEpic.setStatus(TaskStatus.NEW);
+        }
+        for (Integer currentSubTask : currentEpic.getTaskCollection()) {
+            if(!subTasks.get(currentSubTask).getTaskStatus().equals(TaskStatus.NEW)){
+                isEverythingNew = false;
+            }
+            if(!subTasks.get(currentSubTask).getTaskStatus().equals(TaskStatus.DONE)){
+                isEverythingDone = false;
+            }
+        }
+        if(isEverythingNew){
+            currentEpic.setStatus(TaskStatus.NEW);
+        } else if (isEverythingDone) {
+            currentEpic.setStatus(TaskStatus.DONE);
+        } else {
+            currentEpic.setStatus(TaskStatus.IN_PROGRESS);
+        }
+    }
+
+}
