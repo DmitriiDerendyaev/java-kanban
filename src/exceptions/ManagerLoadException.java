b9487e01d54575a6abd39853ceeabb26dd3c569c
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-07-01 19:05:52 +0400

    feature: добавить классы исключения

---
+++ src/exceptions/ManagerLoadException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ManagerLoadException extends RuntimeException {
+
+    public ManagerLoadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
