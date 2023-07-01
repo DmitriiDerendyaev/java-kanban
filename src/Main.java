539f1a660dbaf39ed4bcf33a4d7043467314158e
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-07-01 19:05:25 +0400

    refactor: переделать класс main для обработки тестирования 6го спринта

--- src/Main.java
+++ src/Main.java
@@ -24,95 +24,11 @@ public class Main {
         fileBackedTaskManager.createSubTask(new SubTask("Купить спойлер", "Купить спойлер белого цвета",         // ID = 8
                 TaskStatus.NEW, 2));
 
+        System.out.println(fileBackedTaskManager.getHistory());
+
+
         fileBackedTaskManager.getTaskByID(1);
         fileBackedTaskManager.getEpicByID(2);
         fileBackedTaskManager.getSubTaskByID(3);
-
-        //Основной блок теста по ТЗ #1
-        ///////////////////////////////////////////////////////
-        Task firstTask = new Task("Просто отвертка", "Купить отвертку", TaskStatus.NEW);            // ID = 1
-        Task secondTask = new Task("Просто шуруп", "Купить шуруп", TaskStatus.NEW);         // ID = 2
-        Task thirdTask = new Task("Просто доска", "Купить доску", TaskStatus.NEW);                  // ID = 3
-        System.out.println("Добавление задачи: ID = " + inMemoryTaskManager.createTask(firstTask));
-        System.out.println("Добавление задачи: ID = " + inMemoryTaskManager.createTask(secondTask));
-        System.out.println("Добавление задачи: ID = " + inMemoryTaskManager.createTask(thirdTask));
-
-        Epic firstEpic = new Epic("Купить дом", "Купить пентхаус в Казани");        // ID = 4
-        System.out.println("Добавление эпика: ID = " + inMemoryTaskManager.createEpic(firstEpic));
-
-        SubTask firstSubTask = new SubTask("Материал для пола", "Покупка расходников для пола",
-                TaskStatus.NEW, 4);                                                                                // ID = 5
-        System.out.println("Добавление подзадачи: ID = " + inMemoryTaskManager.createSubTask(firstSubTask));
-
-        Epic secondEpic = new Epic("Купить машину", "Купить Porsche 911");          // ID = 6
-        System.out.println("Добавление эпика: ID = " + inMemoryTaskManager.createEpic(secondEpic));
-
-        SubTask secondSubTask = new SubTask("Купить материал для потолка",
-                "Покупка расходников для потолка", TaskStatus.NEW, 4);                                // ID = 7
-        System.out.println("Добавление подзадачи: ID = " + inMemoryTaskManager.createSubTask(secondSubTask));
-
-        SubTask thirdSubTask = new SubTask("Купить спойлер", "Купить спойлер белого цвета",         // ID = 8
-                TaskStatus.NEW, 6);
-        System.out.println("Добавление подзадачи: ID = " + inMemoryTaskManager.createSubTask(thirdSubTask));
-
-        System.out.println(inMemoryTaskManager.getTaskList());
-        System.out.println(inMemoryTaskManager.getSubTaskList());
-        System.out.println(inMemoryTaskManager.getEpicList());
-        System.out.println("\n");
-        ///////////////////////////////////////////////////////////
-
-        //Обновление задач
-        Task updSecondTask = new Task("Просто шуруп", "Купить шуруп", 2, TaskStatus.IN_PROGRESS);
-        inMemoryTaskManager.updateTask(updSecondTask);
-
-        SubTask updFirstSubTask = new SubTask("Материал для пола", "Покупка расходников для пола",
-                5, TaskStatus.DONE, 4);
-        inMemoryTaskManager.updateSubTask(updFirstSubTask);
-
-        SubTask updSecondSubTask = new SubTask("Купить материал для потолка",
-                "Покупка расходников для потолка", 7, TaskStatus.IN_PROGRESS, 4);
-        inMemoryTaskManager.updateSubTask(updSecondSubTask);
-
-        SubTask updThirdSubTask = new SubTask("Купить спойлер", "Купить спойлер белого цвета",         // ID = 8
-                8, TaskStatus.IN_PROGRESS, 6);
-        inMemoryTaskManager.updateSubTask(updThirdSubTask);
-
-        System.out.println(inMemoryTaskManager.getTaskList());
-        System.out.println(inMemoryTaskManager.getSubTaskList());
-        System.out.println(inMemoryTaskManager.getEpicList());
-        System.out.println("\n");
-        ///////////////////////////////////////////////////////
-
-        //Удаление задач
-        inMemoryTaskManager.removeTaskByID(3);
-        inMemoryTaskManager.removeSubTaskByID(7);
-        inMemoryTaskManager.removeEpicByID(6);
-
-        System.out.println(inMemoryTaskManager.getTaskList());
-        System.out.println(inMemoryTaskManager.getSubTaskList());
-        System.out.println(inMemoryTaskManager.getEpicList());
-        System.out.println("\n");
-
-          ///////////////////////////////////////////////////////
-        // Тестирование ТЗ 4го спринта
-        // Запись в историю
-        HistoryManager inMemoryHistoryManager = Manager.getDefaultHistory();
-
-        System.out.println(inMemoryTaskManager.getTaskByID(2));
-        System.out.println(inMemoryTaskManager.getSubTaskByID(5));
-        System.out.println(inMemoryTaskManager.getEpicByID(4));
-        System.out.println(inMemoryTaskManager.getTaskByID(2));
-        System.out.println(inMemoryTaskManager.getTaskByID(1));
-        System.out.println(inMemoryTaskManager.getTaskByID(2));
-        System.out.println(inMemoryTaskManager.getTaskByID(1));
-        System.out.println(inMemoryTaskManager.getTaskByID(2));
-        System.out.println(inMemoryTaskManager.getTaskByID(1));
-        System.out.println(inMemoryTaskManager.getTaskByID(2));
-        System.out.println(inMemoryTaskManager.getTaskByID(1));
-        System.out.println(inMemoryTaskManager.getTaskByID(2));
-
-        System.out.println(inMemoryTaskManager.getHistory());
-
-
     }
 }
