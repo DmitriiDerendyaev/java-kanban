40828357026b1f3c2c9101b8d3de3f9f5e36b9cf
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-07-25 13:38:50 +0400

    refactor: TODO подготовить main к ручному тестированию

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,37 @@
-import models.Epic;
-import models.SubTask;
-import models.Task;
-import models.TaskStatus;
-import service.FileBackedTasksManager;
-import service.HistoryManager;
-import util.Manager;
-import service.TaskManager;
+    import models.Epic;
+    import models.SubTask;
+    import models.Task;
+    import models.TaskStatus;
+    import service.FileBackedTasksManager;
+    import service.HistoryManager;
+    import util.Manager;
+    import service.TaskManager;
 
-import java.io.IOException;
+    import java.io.IOException;
+    import java.time.Duration;
+    import java.time.ZonedDateTime;
 
-public class Main {
+    public class Main {
 
-    public static void main(String[] args) throws IOException {
+        public static void main(String[] args) throws IOException {
 
-        TaskManager fileBackedTaskManager = new FileBackedTasksManager("src/resources/memory.csv");
+            TaskManager fileBackedTaskManager = new FileBackedTasksManager("src/resources/memory.csv");
 
-        fileBackedTaskManager.createTask(new Task("Просто отвертка", "Купить отвертку", TaskStatus.NEW));
-        fileBackedTaskManager.createEpic(new Epic("Купить дом", "Купить пентхаус в Казани"));
-        fileBackedTaskManager.createSubTask(new SubTask("Материал для пола", "Покупка расходников для пола",
-                TaskStatus.NEW, 2));
-        fileBackedTaskManager.createSubTask(new SubTask("Купить спойлер", "Купить спойлер белого цвета",         // ID = 8
-                TaskStatus.NEW, 2));
+            Duration duration;
+            ZonedDateTime zonedDateTime;
 
-        System.out.println(fileBackedTaskManager.getHistory());
+            fileBackedTaskManager.createTask(new Task("Просто отвертка", "Купить отвертку", TaskStatus.NEW));
+            fileBackedTaskManager.createEpic(new Epic("Купить дом", "Купить пентхаус в Казани"));
+            fileBackedTaskManager.createSubTask(new SubTask("Материал для пола", "Покупка расходников для пола",
+                    TaskStatus.NEW, 2));
+            fileBackedTaskManager.createSubTask(new SubTask("Купить спойлер", "Купить спойлер белого цвета",
+                    TaskStatus.NEW, 2));
+
+            System.out.println(fileBackedTaskManager.getHistory());
 
 
-        fileBackedTaskManager.getTaskByID(1);
-        fileBackedTaskManager.getEpicByID(2);
-        fileBackedTaskManager.getSubTaskByID(3);
+            fileBackedTaskManager.getTaskByID(1);
+            fileBackedTaskManager.getEpicByID(2);
+            fileBackedTaskManager.getSubTaskByID(3);
+        }
     }
-}
