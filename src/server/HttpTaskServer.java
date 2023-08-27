d740403873a5e47ced8fc106e03998979f67738d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-08-28 02:38:48 +0400

    feature: добавить обработчик GET запроса

--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -19,6 +19,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Optional;
 
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -56,26 +58,69 @@ public class HttpTaskServer {
             String method = exchange.getRequestMethod();
 
             try {
-                if (path.equals("/tasks/task") && method.equals("POST")) {
-                    String json = readText(exchange);
-                    Task newTask = gson.fromJson(json, Task.class);
-                    taskManager.createTask(newTask);
-                    System.out.println("Задача успешно добавлена!");
-                    sendText(exchange, "{\"status\":\"OK\"}");
+                if(method.equals("POST")){
+                    if (path.equals("/tasks/task")) {
+                        String json = readText(exchange);
+                        Task newTask = gson.fromJson(json, Task.class);
+                        taskManager.createTask(newTask);
+                        System.out.println("Задача успешно добавлена!");
+                        sendText(exchange, "{\"status\":\"OK\"}");
 
-                } else if (path.equals("/tasks/subTask") && method.equals("POST")) {
-                    String json = readText(exchange);
-                    SubTask newSubTask = gson.fromJson(json, SubTask.class);
-                    taskManager.createSubTask(newSubTask);
-                    System.out.println("Подзадача успешно добавлена!");
-                    sendText(exchange, "{\"status\":\"OK\"}");
+                    } else if (path.equals("/tasks/subTask")) {
+                        String json = readText(exchange);
+                        SubTask newSubTask = gson.fromJson(json, SubTask.class);
+                        taskManager.createSubTask(newSubTask);
+                        System.out.println("Подзадача успешно добавлена!");
+                        sendText(exchange, "{\"status\":\"OK\"}");
 
-                } else if (path.equals("/tasks/epic") && method.equals("POST")) {
-                    String json = readText(exchange);
-                    Epic newEpic = gson.fromJson(json, Epic.class);
-                    taskManager.createEpic(newEpic);
-                    System.out.println("Эпик успешно добавлена!");
-                    sendText(exchange, "{\"status\":\"OK\"}");
+                    } else if (path.equals("/tasks/epic")) {
+                        String json = readText(exchange);
+                        Epic newEpic = gson.fromJson(json, Epic.class);
+                        taskManager.createEpic(newEpic);
+                        System.out.println("Эпик успешно добавлен!");
+                        sendText(exchange, "{\"status\":\"OK\"}");
+                    }
+                } else if (method.equals("GET")) {
+                    Optional<String> query = Optional.ofNullable(exchange.getRequestURI().getQuery());
+                    Boolean isQuery = query.map(q -> q.contains("id")).orElse(false);
+                    if (isQuery) {
+                        int id = Integer.parseInt(exchange.getRequestURI().getQuery().substring("id=".length()));
+
+                        if (path.contains("/tasks/task")) {
+                            Task task = taskManager.getTaskByID(id);
+                            sendText(exchange, gson.toJson(task));
+                        } else if (path.contains("/tasks/subTask")) {
+                            SubTask subTask = taskManager.getSubTaskByID(id);
+                            sendText(exchange, gson.toJson(subTask));
+                        } else if (path.contains("/tasks/epic")) {
+                            Epic epic = taskManager.getEpicByID(id);
+                            sendText(exchange, gson.toJson(epic));
+                        } else if (path.contains("/tasks/epic/subTasks")) {
+                            List<SubTask> subTasks = taskManager.getSubTaskListByEpicID(id);
+                            sendText(exchange, gson.toJson(subTasks));
+                        }
+                    } else {
+                        if (path.equals("/tasks/task")) {
+                            System.out.println("Список задач отправлен на клиент!");
+                            sendText(exchange, gson.toJson(taskManager.getTaskList()));
+                        } else if (path.equals("/tasks/subTask")) {
+                            System.out.println("Список подзадач отправлен на клиент!");
+                            sendText(exchange, gson.toJson(taskManager.getSubTaskList()));
+                        } else if (path.equals("/tasks/epic")) {
+                            System.out.println("Список эпиков отправлен на клиент!");
+                            sendText(exchange, gson.toJson(taskManager.getEpicList()));
+                        } else if (path.equals("/tasks")) {
+                            System.out.println("Задачи отправлены на клиент!");
+                            sendText(exchange, gson.toJson(taskManager.getPrioritizedTask()));
+                        } else if (path.equals("/tasks/history")) {
+                            System.out.println("История отправлена на клиент!");
+                            sendText(exchange, gson.toJson(taskManager.getHistory()));
+                        }
+                    }
+                } else if (method.equals("DELETE")) {
+
+                } else {
+                    throw new IllegalStateException("Неизвестный метод");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -101,6 +146,8 @@ public class HttpTaskServer {
         }
 
         protected void sendText(HttpExchange h, String text) throws IOException {
+            if(text.equals("[]") || text.equals("{}"))
+                text = "Пустой объект";
             byte[] resp = text.getBytes(UTF_8);
             h.getResponseHeaders().add("Content-Type", "application/json");
             h.sendResponseHeaders(200, resp.length);
