8a1d225304582c83dc0cc14bcbc8ce00af44de9c
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-06 21:57:47 +0400

    refactor: убрать лишние методы для фабрики реализаций классов

--- src/util/Manager.java
+++ src/util/Manager.java
@@ -5,20 +5,12 @@ import service.*;
 import java.io.IOException;
 
 public class Manager {
-    public static TaskManager getDefault(){
-        return new InMemoryTaskManager();
-    }
 
     public static HistoryManager getDefaultHistory(){
         return new InMemoryHistoryManager();
     }
 
-    public static FileBackedTasksManager getDefaultFileTaskManager() throws IOException {
-        return new FileBackedTasksManager("src/resources/memory.csv");
-    }
-
-    public static HttpTaskManager getHttpTaskManger() throws IOException, InterruptedException {
+    public static HttpTaskManager getDefault() throws IOException, InterruptedException {
         return new HttpTaskManager("http://localhost:8070");
     }
-
 }
