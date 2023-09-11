39da6763acf04e8bfa4dc18270d42570fb5a0974
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-09-11 11:51:29 +0300

    refactor: добавить конструктор с одним аргументом с посылкой сообщения

--- src/exceptions/ManagerSaveException.java
+++ src/exceptions/ManagerSaveException.java
@@ -5,4 +5,8 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public ManagerSaveException(String message){
+        super(message);
+    }
 }
